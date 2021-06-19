fb1cecb811ea3f9f27f84b16a94486aab69805a0
Nuri Uzunoğlu <nuriu@protonmail.com>
2021-06-19 23:19:53 +0300

    added healthcheck and swagger redirection to api.

--- back-end/Futurity.Api/Startup.cs
+++ back-end/Futurity.Api/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,10 +20,13 @@ namespace Futurity.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Futurity.Api", Version = "v1" });
             });
+
+            services.AddHealthChecks();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -30,15 +34,18 @@ namespace Futurity.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Futurity.Api v1"));
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Futurity.Api v1"));
+
+            app.UseHealthChecks("/healthcheck");
+
+            var rewriteOptions = new RewriteOptions();
+            rewriteOptions.AddRedirect("^$", "swagger");
+            app.UseRewriter(rewriteOptions);
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
